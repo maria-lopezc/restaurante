3022539848cdc8cd2d886d1a434c7a82697de465
maria-lopezc <maria.lopezcaceres1@gmail.com>
2025-07-19 14:35:23 -0300

    cambios de estilo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
